4320f8317723af8dd38277cb3672d3fa8621e1f4
“Leandro” <admin@admin.local>
2023-11-09 22:13:05 -0300

    fix: login path

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 
 export default function Logout() {
   sessionStorage.clear()
-  window.location.assign('login')
+  // window.location.assign('login')
+  window.location.assign('/')
   // const navigate = useNavigate()
   // navigate('./login');
 
